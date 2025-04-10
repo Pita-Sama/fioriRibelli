ee4037a803caafddf92b826604401acde988be8b
Pita-Sama <156099300+Pita-Sama@users.noreply.github.com>
2025-04-10 15:41:54 +0100

    Create fioriRibelliLoading.sql

---
+++ fioriRibelliLoading.sql
@@ -0,0 +1,5 @@
+INSERT INTO categorie (nome, descrizione) VALUES
+('Rose (antiche e moderne)', 'Varietà di rose decorative, antiche e moderne.'),
+("Erbacee Perenni (Sole, Mezz'ombra, Ombra)", 'Piante erbacee perenni adatte a diverse esposizioni.'),
+('Prodotti per la cura delle piante', 'Fertilizzanti, trattamenti e prodotti per la salute delle piante.'),
+('Oggettistiche solidale', 'Articoli solidali e decorativi.');
