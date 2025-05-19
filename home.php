d6cf165b2af027ec744857e0407e390f829b02d3
Davide-netizen <davide.volpi23102006@gmail.com>
2025-05-19 19:53:29 +0200

    Update home.php
    aggiunto il menù tramite il require once

--- home.php
+++ home.php
@@ -14,7 +14,7 @@ require_once("visualizzazione_prodotti.php");
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-
+    <?php require_once 'menu.php'; ?>
     <header>
         <div class="contact-info">
             <span onclick="copyToClipboard(this)">info@fioriribelli.it</span>
