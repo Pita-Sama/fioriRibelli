405f2a5ee6e98204ea5eb74a3d5a5b6fae9de1ae
Pita-Sama <156099300+Pita-Sama@users.noreply.github.com>
2025-05-19 20:44:15 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,26 +1,18 @@
 <?php
 	function invioMail($mailDest, $name, $id){
-        $url = "https://pietroonofriojuniormaiel.altervista.org/projectWork/verification.php?id=$id";
-    	$nomeMittente = "YOURnature";
-        $mailMitt = "no-reply@YOURnature.it";
-        
-        $mailOgg = "Verifica il tuo account";
-        $mailMess = "Ciao $name, clicca qui per verificare l'account:$url";
-        
-        $mailHeaders = "From: " . $nomeMittente . " <" . $mailMitt . ">\r\n";
-        $mailHeaders .= "X-Mailer: PHP/" . phpversion() . "\r\n";
-        $mailHeaders .= "MIME-Version: 1.0\r\n";
-        $mailHeaders .= "Content-Type: text/html; charset=UTF-8\r\n";
-
-        if(mail($mailDest,$mailOgg,$mailMess,$mailHeaders)){
-            // Reindirizza alla pagina di verifica
-            session_start();
-            $_SESSION['pending_email'] = $mailDest;
-            header("Location: verifica.php");
-            exit;
-        }else{
-        	return false;
-        }
-    }
+	        $url = "https://pietroonofriojuniormaiel.altervista.org/projectWork/verification.php?id=$id";
+	    	$nomeMittente = "YOURnature";
+	        $mailMitt = "no-reply@YOURnature.it";
+	        
+	        $mailOgg = "Verifica il tuo account";
+	        $mailMess = "Ciao $name, clicca qui per verificare l'account:$url";
+	        
+	        $mailHeaders = "From: " . $nomeMittente . " <" . $mailMitt . ">\r\n";
+	        $mailHeaders .= "X-Mailer: PHP/" . phpversion() . "\r\n";
+	        $mailHeaders .= "MIME-Version: 1.0\r\n";
+	        $mailHeaders .= "Content-Type: text/html; charset=UTF-8\r\n";
+	
+	        mail($mailDest,$mailOgg,$mailMess,$mailHeaders));
+	}
 
 ?>
